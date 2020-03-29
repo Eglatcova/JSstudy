ce28ea6acd32bd533be4048a03f6db13528f7f07
Eglatcova <e.glatcova@yandex.ru>
2020-03-30 02:16:48 +0300

    Добавила переменные

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let money = 1000;
+let income = 10000;
+let addExpenses = 550;
+let deposit = 3000;
+let mission = 2780;
+let period = 7;
+
+alert('Привет!');
+console.log(deposit);
+console.log('ok');
