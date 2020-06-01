55e15a2a1abb9d8a49abb8ac1fc029196eacb4c2
Eglatcova <e.glatcova@yandex.ru>
2020-06-01 18:14:53 +0300

    LESSON27

--- script/script.js
+++ script/script.js
@@ -376,37 +376,26 @@ window.addEventListener("DOMContentLoaded", () => {
       });
 
       const postData = (body) => {
-        return new Promise((resolve, reject) => {
-          const request = new XMLHttpRequest();
-          //важно вешать прослушку сразу после создания request
-          request.addEventListener("readystatechange", () => {
-            if (request.readyState !== 4) {
-              return;
-            }
-            if (request.status === 200) {
-              resolve();
-            } else {
-              reject(request.status);
-            }
-          });
-
-          request.open("POST", "./server.php");
-          request.setRequestHeader("Content-Type", "application/json");
-
-          //отправка данных в JSON формате
-          request.send(JSON.stringify(body));
+        return fetch("./server.php", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
         });
       };
 
       postData(body)
-        .then(() => {
+        .then((response) => {
+          if (response.status !== 200) {
+            throw new Error("status network not 200");
+          }
           statusMessage.textContent = successMessage;
           inputs.forEach((elem) => {
             elem.value = "";
           });
         })
-        .catch(() => {
+        .catch((error) => {
           statusMessage.textContent = errorMessage;
+          console.error(error);
         });
     });
   };
@@ -449,37 +438,26 @@ window.addEventListener("DOMContentLoaded", () => {
       });
 
       const postData = (body) => {
-        return new Promise((resolve, reject) => {
-          const request = new XMLHttpRequest();
-          //важно вешать прослушку сразу после создания request
-          request.addEventListener("readystatechange", () => {
-            if (request.readyState !== 4) {
-              return;
-            }
-            if (request.status === 200) {
-              resolve();
-            } else {
-              reject(request.status);
-            }
-          });
-
-          request.open("POST", "./server.php");
-          request.setRequestHeader("Content-Type", "application/json");
-
-          //отправка данных в JSON формате
-          request.send(JSON.stringify(body));
+        return fetch("./server.php", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
         });
       };
 
       postData(body)
-        .then(() => {
+        .then((response) => {
+          if (response.status !== 200) {
+            throw new Error("status network not 200");
+          }
           statusMessage.textContent = successMessage;
           inputs.forEach((elem) => {
             elem.value = "";
           });
         })
-        .catch(() => {
+        .catch((error) => {
           statusMessage.textContent = errorMessage;
+          console.error(error);
         });
     });
   };
@@ -518,37 +496,26 @@ window.addEventListener("DOMContentLoaded", () => {
       });
 
       const postData = (body) => {
-        return new Promise((resolve, reject) => {
-          const request = new XMLHttpRequest();
-          //важно вешать прослушку сразу после создания request
-          request.addEventListener("readystatechange", () => {
-            if (request.readyState !== 4) {
-              return;
-            }
-            if (request.status === 200) {
-              resolve();
-            } else {
-              reject(request.status);
-            }
-          });
-
-          request.open("POST", "./server.php");
-          request.setRequestHeader("Content-Type", "application/json");
-
-          //отправка данных в JSON формате
-          request.send(JSON.stringify(body));
+        return fetch("./server.php", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
         });
       };
 
       postData(body)
-        .then(() => {
+        .then((response) => {
+          if (response.status !== 200) {
+            throw new Error("status network not 200");
+          }
           statusMessage.textContent = successMessage;
           inputs.forEach((elem) => {
             elem.value = "";
           });
         })
-        .catch(() => {
+        .catch((error) => {
           statusMessage.textContent = errorMessage;
+          console.error(error);
         });
     });
   };
